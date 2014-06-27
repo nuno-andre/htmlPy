009d4a25ddfbb3c066df65017f7f8d393f659163
Amol Mandhane <manamol296@gmail.com>
2014-06-27 22:45:08 +0530

    Write bridge tests

--- tests/test_app_window_bridge.py
+++ tests/test_app_window_bridge.py
@@ -1,11 +1,12 @@
 import unittest
 import htmlPy
 import os
+import json
 
 class TestAppWindowBridge(unittest.TestCase):
 
     def setUp(self):
-        self.app = htmlPy.AppWindow(title="Test", width=40, height=80, x_pos=10, y_pos=20)
+        self.app = htmlPy.AppWindow(developer_mode=True)
         self.test_template = "index.html"
         self.test_context = {"appname": "htmlPy"}
         self.app.set_template_path(os.path.abspath(os.path.dirname(__file__)))
@@ -30,7 +31,31 @@ class TestAppWindowBridge(unittest.TestCase):
             def change_custom(self, name):
                 app.set_template(template, {"appname": name})
 
+            @htmlPy.attach(str)
+            def form_batman(self, data):
+                data = json.loads(str(data))
+                app.set_template(template, {"appname": data["name"]})
+
+            @htmlPy.attach(str, str)
+            def form_joker(self, data, params):
+                data = json.loads(str(data))
+                app.set_template(template, {"appname": data["name"] + ", " + str(params)})
+
         self.test_bridge = BridgeCheck()
+
+    def manually_execute_bridge_helper_javascript(self, appl):
+        # In automated testing, this has to be done manually
+        # When in dev or production, the function loads 100ms after
+        # the window starts. It is done by javascript setInterval
+        script_path = "htmlPy/bridge_helper.min.js"
+        try:
+            f = open(script_path, "r")
+        except:
+            f = open("../" + script_path, "r")
+        script = f.read()
+        script = script.replace("setInterval(function(){", "")
+        script = script.replace("},100)", "")
+        appl.execute_javascript(script)
 
     def test_template_rendering(self):
         html = self.app.render_template(self.test_template, self.test_context)
@@ -54,38 +79,69 @@ class TestAppWindowBridge(unittest.TestCase):
         assert "file:///" + css_path in self.app.get_html()
 
     def test_register(self):
-        self.app.set_template(self.test_template, self.test_context)
         self.app.register(self.test_bridge)
-
+        self.app.set_template(self.test_template, self.test_context)
         self.app.execute_javascript("BridgeCheck.change_superman()")
         assert "<h1>Superman" in self.app.get_html()
 
     def test_bridge_retention(self):
-        self.app.set_template(self.test_template, self.test_context)
         self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
 
         self.app.execute_javascript("BridgeCheck.change_superman()")
         self.app.execute_javascript("BridgeCheck.change_batman()")
         assert "<h1>Batman" in self.app.get_html()
 
     def test_bridge_function_arguments(self):
-        self.app.set_template(self.test_template, self.test_context)
         self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
         self.app.execute_javascript("BridgeCheck.change_custom('Iron man')")
         assert "<h1>Iron man" in self.app.get_html()
 
     def test_bridge_function_return_vals(self):
-        self.app.set_template(self.test_template, self.test_context)
         self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
         self.app.execute_javascript("document.write(BridgeCheck.change_deathstroke())")
         assert "Deathstroke" in self.app.get_html()
 
-
     def test_link_click(self):
-        pass
+        self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
+        self.manually_execute_bridge_helper_javascript(self.app)
+        self.app.execute_javascript("document.getElementById('test_link_click').click()")
+        assert "<h1>Superman" in self.app.get_html()
+
+    def test_link_click_params(self):
+        self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
+        self.manually_execute_bridge_helper_javascript(self.app)
+        self.app.execute_javascript("document.getElementById('test_link_click_params').click()")
+        assert "<h1>Iron man" in self.app.get_html()
+
+    def test_link_click_404(self):
+        self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
+        self.manually_execute_bridge_helper_javascript(self.app)
+        html = self.app.get_html()
+        self.app.execute_javascript("document.getElementById('test_link_click_404').click()")
+        assert html == self.app.get_html()
 
     def test_form_submit(self):
-        pass
+        self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
+        self.manually_execute_bridge_helper_javascript(self.app)
+        self.app.execute_javascript("document.getElementById('test_form_submit_input').value='Python'")
+        self.app.execute_javascript("document.getElementById('test_form_submit_button').click()")
+        assert "<h1>Python" in self.app.get_html()
+
+    def test_form_submit_params(self):
+        self.app.register(self.test_bridge)
+        self.app.set_template(self.test_template, self.test_context)
+        self.manually_execute_bridge_helper_javascript(self.app)
+        self.app.execute_javascript("document.getElementById('test_form_submit_params_input').value='Batman'")
+        self.app.execute_javascript("document.getElementById('test_form_submit_params_button').click()")
+        # Note: Javascript form.submit() does not call onsubmit callback
+        assert "<h1>Batman, Why so serious?" in self.app.get_html()
 
 
 if __name__ == '__main__':
